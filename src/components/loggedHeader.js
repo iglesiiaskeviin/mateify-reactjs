7a0e33bca77ab4eb834ae3dd319e7e4b36ac46b1
Kevin <eiikevin@gmail.com>
2020-08-13 16:22:29 -0300

    Programmed some functions for login form

--- src/components/loggedHeader.js
+++ src/components/loggedHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import UserImg from '../databases/userData.json';
 
 export const LoggedHeader = props => {
     return (
@@ -7,7 +7,9 @@ export const LoggedHeader = props => {
             <img className="user-photo" src="https://cdn.discordapp.com/attachments/702899679947522121/714933469796499567/LogoSoloBlancoFondoTransparente.png" alt=""/>
             <h1 className="mateify-h1">Mateify</h1>
             <form action="" className="in-line">
-                <img className="user-photo" src={props.userPhoto} alt=""/>
+            {UserImg.map((img, index)=>{
+                return <img  className="user-photo" src={img.profilePictureUrl} alt=""/>
+                })}
             </form>
         </nav>
     )
--- src/pages/login.js
+++ src/pages/login.js
@@ -5,7 +5,26 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 import {Button} from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
-export const Login = () => {
+class Form extends React.Component {
+
+    state = {
+        email: '',
+        password: '',
+    };
+
+    change = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    render(){
     return (
     <div className="col-xs">
         <LoginHeader CoName="Mateify" CoLogoImg="https://cdn.discordapp.com/attachments/702899679947522121/714933469796499567/LogoSoloBlancoFondoTransparente.png"/>
@@ -16,15 +35,35 @@ export const Login = () => {
                     <h1 className="logIn-h11">Ingresar</h1>
                 </div>
                 <div className="form-group col-lg">
-                    <input type="text" class="form-control" id="" placeholder="Correo Electrónico"/>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    name="email"
+                    placeholder="Correo Electrónico" 
+                    value={this.state.email}
+                    onChange={e=> this.setState({email: e.target.value})}
+                />
                 </div>
                 <div className="form-group col-lg">
-                    <input type="password" class="form-control" id="" placeholder="Contraseña"/>
+                    <input 
+                    type="password" 
+                    class="form-control" 
+                    name="password"
+                    placeholder="Contraseña" 
+                    value={this.state.password} 
+                    onChange={e=> this.setState({password: e.target.value})}
+                    />
                 </div>
                 <div className="text-center">
                     <div className="col-lg">
-                    <Button variant="contained" className="btn-create" color="secondary" type="submit">
-                        Comienza a crear playlists
+                    <Button 
+                    variant="contained" 
+                    className="btn-create" 
+                    color="secondary" 
+                    type="submit"
+                    onClick={ e => this.onSubmit(e)}
+                    >
+                    Comienza a crear playlists
                     </Button>
                     </div>
                 </div>
@@ -52,46 +91,6 @@ export const Login = () => {
 
     )
 }
+}
 
-
-/* 
-        <div className="text-center main-section">
-            <LoginHeader CoName="Mateify" CoLogoImg="https://cdn.discordapp.com/attachments/702899679947522121/714933469796499567/LogoSoloBlancoFondoTransparente.png"/>
-            <div className="col-xs-6 col-lg-5 mx-auto my-auto justify-content-center align-items-center row">
-                <div className="col-12 container-logo">
-                    <LockOutlinedIcon color="secondary" />
-                </div>
-                <h1 className="logIn-h11">Ingresar</h1>
-                <div className="form-group col-12">
-                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Correo Electrónico"/>
-                </div>
-                <div className="form-group col-12">
-                    <input type="password" class="form-control" id="exampleInputEmail1" placeholder="Contraseña"/>
-                </div>
-                <div className="col-12">
-                <Button variant="contained" color="secondary" type="submit">
-                    Comienza a crear playlists
-                </Button>
-                </div>
-                <div className="col-12 my-4 not-remember-pass-container">
-                    <p>¿Has olvidado la contraseña?</p>
-                </div>
-                <hr/>
-                <div className="col-12 my-4">
-                    <p className="not-account">¿No tienes cuenta?</p>
-                    <Button selected style={{
-                        borderRadius: 4,
-                        color: "#009688",
-                        fontSize: "20",
-                        fontFamily: 'Roboto',
-                        fontWeight: "500",
-                    }} 
-                    variant="outlined" 
-                    type="submit">
-                    REGISTRATE
-                    </Button>
-                </div>
-            </div>
-        </div>
-
-*/
+export default Form;
