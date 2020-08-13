07e2df9c1c95310bb2cc697094c35ddef59b50e8
Kevin <eiikevin@gmail.com>
2020-08-13 16:22:29 -0300

    Programmed some functions for login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
-import {Login} from '../src/pages/login.js';
+import Form from '../src/pages/login.js';
 import {LoginHeader} from './components/loginHeader.js';
 import {LoggedHeader} from './components/loggedHeader.js';
 
 function App() {
   return (
-    <Login />
+    <Form />
   );
 }
 
---
+++ src/components/checkUser.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import userData from '../databases/userData.json';
+
+
+
+
+class CheckUser extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          userData,
+      };
+    }
+
+    
+  
+    render() {
+      return (
+        <div>
+            <ul>
+                {
+                    this.state.userData.map((persons)=>{
+                        return (
+                            <li key={persons.uuid}>{persons.email}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+      );
+}
+}
+
+export default CheckUser;
