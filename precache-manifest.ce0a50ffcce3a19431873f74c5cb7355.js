96f07cdf1b7854f80c3d39f2b1af03ed61707940
Kevin <eiikevin@gmail.com>
2020-08-26 17:36:27 -0300

    Updates

---
+++ precache-manifest.ce0a50ffcce3a19431873f74c5cb7355.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64d8cc3f944cc76b4b727d6c73b2a86",
+    "url": "/%7Bmateify-reactjs%7D/index.html"
+  },
+  {
+    "revision": "112e1689299c6a39d605",
+    "url": "/%7Bmateify-reactjs%7D/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "0cb78197877f23d8f61f",
+    "url": "/%7Bmateify-reactjs%7D/static/css/main.9598f0b4.chunk.css"
+  },
+  {
+    "revision": "112e1689299c6a39d605",
+    "url": "/%7Bmateify-reactjs%7D/static/js/2.08056990.chunk.js"
+  },
+  {
+    "revision": "292c0dcab4b2aa797006a497ac447956",
+    "url": "/%7Bmateify-reactjs%7D/static/js/2.08056990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb78197877f23d8f61f",
+    "url": "/%7Bmateify-reactjs%7D/static/js/main.fe63fb31.chunk.js"
+  },
+  {
+    "revision": "e4a85e57d7837a1579ca",
+    "url": "/%7Bmateify-reactjs%7D/static/js/runtime-main.b886bd68.js"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/%7Bmateify-reactjs%7D/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "490cb0e45f39580c7ba1b856f2f1f4a7",
+    "url": "/%7Bmateify-reactjs%7D/static/media/righteous.regular.490cb0e4.ttf"
+  }
+]);
